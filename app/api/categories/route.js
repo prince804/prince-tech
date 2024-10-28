79ef4c600ea476d8e869a09d4ef235f0eab27bb3
NawabMoazzam <cnstatus310@gmail.com>
2024-10-28 16:20:55 +0500

    Updated

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -9,19 +9,57 @@ const getCategories = async () => {
     return data;
 }
 
+const getPosts = async (categoryId) => {
+    let fetchedPosts = [];
+    let page = 1;
+    let totalPages = 1;
+
+    do {
+        const response = await fetch(`https://ketosl.com/wp-json/wp/v2/posts?categories=${categoryId}&per_page=100&page=${page}&_fields=title,link`);
+        const data = await response.json();
+
+        // Map the fetched posts from the current page
+        const postsFromPage = data.map(post => ({
+            title: post.title.rendered,
+            link: post.link
+        }));
+
+        // Add the current page posts to the fetchedPosts array
+        fetchedPosts = fetchedPosts.concat(postsFromPage);
+
+        // Get the total number of pages from the headers
+        totalPages = response.headers.get('X-WP-TotalPages');
+        page++;
+    } while (page <= totalPages);
+
+    return fetchedPosts;
+}
+
 export async function POST() {
     revalidateTag("categories");
     try {
         await dbConnect();
         const Categories = await getCategories();
         
-        const bulkOps = Categories.map(category => ({
-            updateOne: {
-                filter: { categoryId: category.id },
-                update: { categoryId: category.id, name: category.name, posts: [] },
-                upsert: true // This will insert if not found
-            }
-        }));
+        const bulkOps = await Promise.all(
+            Categories.map(async (category) => {
+                const posts = await getPosts(category.id); // Fetch posts for each category
+
+                return {
+                    updateOne: {
+                        filter: { categoryId: category.id },
+                        update: {
+                            $setOnInsert: { // Only set these fields if the document is new
+                                categoryId: category.id,
+                                name: category.name,
+                                posts: posts
+                            }
+                        },
+                        upsert: true // Insert if not found
+                    }
+                };
+            })
+        );
 
         await Category.bulkWrite(bulkOps); // Perform the bulk write operation
         const newCategoryIds = Categories.map(category => category.id);
