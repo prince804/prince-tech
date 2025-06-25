460bcc4fe3b0b2d9db1f65519305ab690c786bdc
prince804 <161753125+prince804@users.noreply.github.com>
2025-06-25 11:37:16 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/go',
+        has: [
+          {
+            type: 'query',
+            key: 'utm_campaign',
+          },
+        ],
+        destination: '/dashboard/:utm_campaign',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
